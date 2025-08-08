3ff049c2d7b5c8e3af270ab8a16cfe5eeaf30a1f
Giovanni Melo <web.gcm@gmail.com>
2025-08-08 20:16:29 -0300

    Setup Hilt and define MovieService API endpoints
    - Added `MovieApp` Application class and annotated with `@HiltAndroidApp`.
    - Initialized Timber for logging in debug builds within `MovieApp`.
    - Updated `AndroidManifest.xml` to use the `MovieApp` Application class.
    - Annotated `MainActivity` with `@AndroidEntryPoint`.
    - Created `MovieService` interface defining API endpoints for popular movies, movie search, movie details, and similar movies using Retrofit annotations.
    - Implemented `ParamsInterceptor` to add common query parameters (`api_key`, `language`) to all API requests.
    - Defined constants for API key and language parameters in `Constants.kt`.

---
+++ app/src/main/java/br/com/movieapp/core/data/remote/ParamsInterceptor.kt
@@ -0,0 +1,22 @@
+package br.com.movieapp.core.data.remote
+
+import br.com.movieapp.BuildConfig
+import br.com.movieapp.core.data.util.API_KEY_PARAM
+import br.com.movieapp.core.data.util.LANGUAGE_PARAM
+import br.com.movieapp.core.data.util.LANGUAGE_VALUE
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class ParamsInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val url = request.url.newBuilder()
+            .addQueryParameter(LANGUAGE_PARAM, LANGUAGE_VALUE)
+            .addQueryParameter(API_KEY_PARAM, BuildConfig.API_KEY)
+            .build()
+        val newRequest = request.newBuilder()
+            .url(url)
+            .build()
+        return chain.proceed(newRequest)
+    }
+}
---
+++ app/src/main/java/br/com/movieapp/core/data/util/Constants.kt
@@ -0,0 +1,5 @@
+package br.com.movieapp.core.data.util
+
+const val API_KEY_PARAM = "api_key"
+const val LANGUAGE_PARAM = "language"
+const val LANGUAGE_VALUE = "pt-BR"
