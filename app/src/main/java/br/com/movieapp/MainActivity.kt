e4916d8b6f7edd99c825d75cdbabc9f3069a926e
Giovanni Melo <web.gcm@gmail.com>
2025-08-08 23:28:52 -0300

    Refactor: Adjust import paths after package restructure
    - Updated import paths in various files to reflect the move of data and presentation layers from `framework` to `core`.
      - `MovieDetailResponse.kt`
      - `MovieService.kt`
      - `NetworkModule.kt`
      - `MainActivity.kt`
      - `SearchResponse.kt`
      - `MovieResponse.kt`

--- app/src/main/java/br/com/movieapp/MainActivity.kt
+++ app/src/main/java/br/com/movieapp/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
-import br.com.movieapp.framework.presentation.MainScreen
+import br.com.movieapp.core.presentation.MainScreen
 import br.com.movieapp.ui.theme.MovieAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
